131fdb9af11b89cad7c30dfb2eb5d5db25ccbaf7
mpolanco-ifyl <123343881+mpolanco-ifyl@users.noreply.github.com>
2023-03-02 06:41:03 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,78 +2,53 @@ import openai
 import streamlit as st
 import os
 
-# Inicializa el modelo GPT-3
+# Set up the OpenAI API key from the environment variable
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-   
 # Function to send a message to the OpenAI chatbot model and return its response
 def send_message(message_log):
     # Use OpenAI's ChatCompletion API to get the chatbot's response
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",  # The name of the OpenAI chatbot model to use
-        messages=message_log,   # The conversation history up to this point, as a list of dictionaries
-        max_tokens=4096,        # The maximum number of tokens (words or subwords) in the generated response
-        stop=None,              # The stopping sequence for the generated response, if any (not used here)
-        temperature=0.7,        # The "creativity" of the generated response (higher temperature = more creative)
+    response = openai.Completion.chat.create(
+        engine="chat-3.5-turbo",      # The name of the OpenAI chatbot model to use
+        prompt=message_log,    # The conversation history up to this point, as a string
+        max_tokens=4024,       # The maximum number of tokens (words or subwords) in the generated response
+        stop=None,             # The stopping sequence for the generated response, if any (not used here)
+        temperature=0.7,       # The "creativity" of the generated response (higher temperature = more creative)
     )
 
-    # Find the first response from the chatbot that has text in it (some responses may not have text)
-    for choice in response.choices:
-        if "text" in choice:
-            return choice.text
-
-    # If no response with text is found, return the first response's content (which may be empty)
-    return response.choices[0].message.content
-
+    # Get the generated response from the API response
+    return response.choices[0].text
 
 # Main function that runs the chatbot
 def main():
-    # Initialize the conversation history with a message from the chatbot
-    message_log = [
-        {"role": "system", "content": "You are a helpful assistant."}
-    ]
+    # Add a sidebar with a text input box for the API key
+    st.sidebar.title("OpenAI Chatbot")
+    api_key = st.sidebar.text_input("Enter your OpenAI API key", type="password")
 
-    # Set a flag to keep track of whether this is the first request in the conversation
-    first_request = True
+    # Set up the OpenAI API key from the input value
+    openai.api_key = api_key
+
+    # Initialize the conversation history with a message from the chatbot
+    message_log = "You are a helpful assistant.\n"
 
     # Start a loop that runs until the user types "quit"
     while True:
-        if first_request:
-            # If this is the first request, get the user's input and add it to the conversation history
-            user_input = input("You: ")
-            message_log.append({"role": "user", "content": user_input})
+        # Get the user's input and add it to the conversation history
+        user_input = st.text_input("You:", "")
+        message_log += f"User: {user_input}\n"
 
-            # Add a message from the chatbot to the conversation history
-            message_log.append({"role": "assistant", "content": "You are a helpful assistant."})
+        # If the user types "quit", end the loop and print a goodbye message
+        if user_input.lower() == "quit":
+            st.write("Goodbye!")
+            break
 
-            # Send the conversation history to the chatbot and get its response
-            response = send_message(message_log)
+        # Send the conversation history to the chatbot and get its response
+        response = send_message(message_log)
 
-            # Add the chatbot's response to the conversation history and print it to the console
-            message_log.append({"role": "assistant", "content": response})
-            print(f"AI assistant: {response}")
+        # Add the chatbot's response to the conversation history and print it to the console
+        message_log += f"Chatbot: {response}\n"
+        st.write("Chatbot:", response)
 
-            # Set the flag to False so that this branch is not executed again
-            first_request = False
-        else:
-            # If this is not the first request, get the user's input and add it to the conversation history
-            user_input = input("You: ")
-
-            # If the user types "quit", end the loop and print a goodbye message
-            if user_input.lower() == "quit":
-                print("Goodbye!")
-                break
-
-            message_log.append({"role": "user", "content": user_input})
-
-            # Send the conversation history to the chatbot and get its response
-            response = send_message(message_log)
-
-            # Add the chatbot's response to the conversation history and print it to the console
-            message_log.append({"role": "assistant", "content": response})
-            print(f"AI assistant: {response}")
-
-
-# Call the main function if this file is executed directly (not imported as a module)
+# Call the main function
 if __name__ == "__main__":
     main()
